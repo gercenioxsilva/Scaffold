d04c9afc5ac14e62bdfe3a2c102f6c29906e23c7
Gercenio Xavier / Software Engineer <gercenio@gmail.com>
2023-01-11 16:31:28 -0300

    add new class

---
+++ src/Scaffold.CrossCutting/ConfigurationSettings/ConfigurationManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Scaffold.CrossCutting.ConfigurationSettings
+{
+    public static class ConfigurationManager
+    {
+        public static IServiceCollection AddConfigurationManager(this IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", true, true)
+               .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json", true, true)
+               .Build();
+
+            return services;
+        }
+    }
+}
---
+++ src/Scaffold.CrossCutting/DependecyInjector/RepositoryServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Scaffold.CrossCutting.DependecyInjector
+{
+    public static class RepositoryServiceCollectionExtension
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services) 
+        {
+            return services;
+        }
+    }
+}
