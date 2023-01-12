01be9c2b626f09f74d013a564b02f0f7f6f06dee
Gercenio Xavier / Software Engineer <gercenio@gmail.com>
2023-01-12 09:26:38 -0300

    Add validations

---
+++ src/Scaffold.CrossCutting/DependecyInjector/MediatorServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Scaffold.CrossCutting.DependecyInjector
+{
+    public static class MediatorServiceCollectionExtension
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services)
+        {
+            var assembly = AppDomain.CurrentDomain.Load("Scaffold.Application");
+
+            AssemblyScanner
+                .FindValidatorsInAssembly(assembly)
+                .ForEach(result => services.AddScoped(result.InterfaceType, result.ValidatorType));
+
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(FailFastRequestBehavior<,>));
+
+            services.AddMediatR(assembly);
+            return services;
+        }
+    }
+}
