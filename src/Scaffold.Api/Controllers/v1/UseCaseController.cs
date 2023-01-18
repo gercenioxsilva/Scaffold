7d522659bbb0ee36686b7daab6a7eb84861adcad
Gercenio Xavier / Software Engineer <gercenio@gmail.com>
2023-01-18 13:59:25 -0300

    fix: implement controllers

---
+++ src/Scaffold.Api/Controllers/v1/UseCaseController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Scaffold.Application.Handlers;
+
+namespace Scaffold.Api.Controllers.v1
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1")]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class UseCaseController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(UseCaseRequest), StatusCodes.Status201Created)]
+        public async Task<ActionResult> Create([FromServices] IMediator _mediator, [FromBody] UseCaseRequest request)
+            => Ok(_mediator.Send(request));
+    }
+}
--- src/Scaffold.Api/Program.cs
+++ src/Scaffold.Api/Program.cs
@@ -1,15 +1,47 @@
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Scaffold.CrossCutting.DependecyInjector;
+using Scaffold.CrossCutting.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediator();
+builder.Services.AddLogger();
+builder.Services.AddRepository();
+builder.Services.AddHealthCheckConfigurations();
+builder.Services.AddSwaggerGen(options => 
+{
+    options.CustomSchemaIds(type => type.ToString());
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() 
+    {
+        Title = "Scaffold Api DotNet Core version 1.0",
+        Version= "v1",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact() 
+        {
+            Email = "gercenio@gmail.com",
+            Name = "Gercenio Xavier"
+        }
+    });
+});
+builder.Services.AddApiVersioning(opt => 
+{
+    opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+    opt.AssumeDefaultVersionWhenUnspecified = true;
+    opt.ReportApiVersions = true;
+    opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("x-api-version"),
+        new MediaTypeApiVersionReader("x-api-version"));
+});
+builder.Services.AddVersionedApiExplorer(setup => 
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
